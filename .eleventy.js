20bed23c31e7958188cc5c6ddaec23177e1da5c9
Jorge Díaz <jorge@fidel.uk>
2020-09-19 18:45:41 +0100

    fix(alpine): remove alpinejs

--- .eleventy.js
+++ .eleventy.js
@@ -10,10 +10,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' });
 
-  eleventyConfig.addPassthroughCopy({
-    './node_modules/alpinejs/dist/alpine.js': './js/alpine.js',
-  });
-
   // PWA files
   eleventyConfig.addPassthroughCopy({
     './manifest.webmanifest': './manifest.webmanifest',
