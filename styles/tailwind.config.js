633e1497732d063be73eb0fb71a66ec77cf56dc1
Jorge Díaz <jorge@fidel.uk>
2020-09-20 14:21:30 +0100

    fix(pagination): add pagination on blog posts

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -19,8 +19,14 @@ module.exports = {
         yellow: '#fff1a5',
         black: '#444',
       },
+      margin: {
+        1.5: '0.4rem',
+      },
       maxHeight: {
         48: '12rem',
+      },
+      inset: {
+        '-8': '-2rem',
       },
       zIndex: {
         '-1': '-1',
