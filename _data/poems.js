b12f2a69c95098657e4047655316b0c4e90933d3
Jorge Díaz <diazproduction@hotmail.es>
2020-09-25 20:16:39 +0100

    feat(navigation): add navigation

---
+++ _data/poems.js
@@ -0,0 +1,6 @@
+const getArticles = require('../utils/getArticles');
+
+module.exports = async function () {
+  const articles = await getArticles();
+  return articles.filter((article) => article.isPoem);
+};
